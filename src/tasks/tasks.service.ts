0901b5fe429a4093e25e0b6584c646ecf10717cf
Kingsleyokoro <50120436+Kingsleyokoro@users.noreply.github.com>
2021-02-06 00:07:33 -0700

    Creatd multiple end points

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 // eslint-disable-next-line prettier/prettier
 import { Task, TaskStatus} from './task.model'
 import * as uuid from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -11,10 +12,17 @@ export class TasksService {
     return this.tasks;
   }
 
-  createTask(title: string, description: string): Task {
+  getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    // eslint-disable-next-line prettier/prettier
+    const {title, description} = createTaskDto;
+
     const task: Task = {
-      id: uuid(),
+      id: uuid.v1(),
       title: title,
       description: description,
       status: TaskStatus.OPEN,
